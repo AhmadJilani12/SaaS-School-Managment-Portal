db427c445d3b7842831f3f1bfa04e54af60f893c
My8921 <ahmadali43a5@gmail.com>
2025-11-05 13:49:20 +0500

    setted login page to default

--- school-portal/pages/index.js
+++ school-portal/pages/index.js
@@ -1,9 +1,8 @@
+import LoginPage from "./login";
+
 export default function Home() {
   return (
-    <main style={{fontFamily:'Arial, sans-serif',padding:40}}>
-      <h1>School Portal (Next.js)</h1>
-      <p>This is a minimal Next.js scaffold for the School Management MVP.</p>
-      <p>API test: <a href="/api/hello">/api/hello</a></p>
-    </main>
+      <LoginPage></LoginPage>
+   
   )
 }
