c4e750b8656adc29899cbf90196de0c6ce13fb31
My8921 <ahmadali43a5@gmail.com>
2025-12-09 15:29:14 +0500

    udpated users models

--- school-portal/models/User.js
+++ school-portal/models/User.js
@@ -2,13 +2,24 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  roles: [{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }],
+  firstName: { type: String, required: true },
+  lastName:  { type: String, required: true },
+  email:     { type: String, unique: true, required: true },
+  password:  { type: String, required: true },
+
+  roles: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Role" }
+  ],
+
+  isActive: {
+    type: Boolean,
+    default: true
+  }
 });
+
 export default mongoose.models.User ||
   mongoose.model("User", UserSchema);
+
 
 
 
